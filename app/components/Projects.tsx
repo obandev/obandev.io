d85a673da2bfe61516e5643513c293edcabcd194
jose pablo <josepabloobandogonzalez@gmail.com>
2023-09-26 16:52:18 -0600

    feat: test link

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -2,41 +2,40 @@
 import projects from 'app/utils/projects'
 import clsx from 'clsx'
 import Image from 'next/image'
-// import { useState } from 'react'
+import { useState } from 'react'
 
 export default function Projects() {
-  // const [isHovered, setIsHovered] = useState(false)
-  // const [hovered, setHovered] = useState(10)
+  const [isHovered, setIsHovered] = useState(false)
+  const [hovered, setHovered] = useState(10)
 
   return (
     <div
       className='columns-2 sm:columns-3 gap-4 my-8'
-      // onMouseEnter={() => setIsHovered(true)}
-      // onMouseLeave={() => setIsHovered(false)}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
       {projects.map((p, i) => (
-        <a
-          href={p.href}
-          target='_blank'
-          rel='noopener noreferrer'
-          key={i}
-          // onMouseEnter={() => setHovered(i)}
-          // onMouseLeave={() => setHovered(10)}
-        >
-          <div className={p.wrapperClass}>
-            <Image
-              alt={p.alt}
-              src={p.src}
-              fill
-              sizes={p.sizes}
-              priority
-              className={clsx(
-                p.imgClass
-                // {'md:opacity-50': isHovered && hovered !== i, }
-              )}
-            />
-          </div>
-        </a>
+        // <a
+        //   href={p.href}
+        //   target='_blank'
+        //   rel='noopener noreferrer'
+        //   key={i}
+        //   onMouseEnter={() => setHovered(i)}
+        //   onMouseLeave={() => setHovered(10)}
+        // >
+        <div className={p.wrapperClass}>
+          <Image
+            alt={p.alt}
+            src={p.src}
+            fill
+            sizes={p.sizes}
+            priority
+            className={clsx(p.imgClass, {
+              'md:opacity-50': isHovered && hovered !== i,
+            })}
+          />
+        </div>
+        // </a>
       ))}
     </div>
   )
