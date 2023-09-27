357a43756816386f21abc94a8914b046acf7dc20
jose pablo <josepabloobandogonzalez@gmail.com>
2023-09-27 07:56:17 -0600

    feat: add test flex

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -10,34 +10,33 @@ export default function Projects() {
 
   return (
     <div
-      className='columns-2 sm:columns-3 gap-4 my-8'
+      className='flex flex-row flex-wrap sm:columns-3 gap-4 my-8'
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      style={{
-        columns: '120px',
-      }}
     >
       {projects.map((p, i) => (
-        <a
-          href={p.href}
-          target='_blank'
-          rel='noopener noreferrer'
-          key={i}
-          onMouseEnter={() => setHovered(i)}
-          onMouseLeave={() => setHovered(10)}
-        >
-          <div className={p.wrapperClass}>
-            <Image
-              alt={p.alt}
-              src={p.src}
-              fill
-              priority
-              className={clsx(p.imgClass, {
-                'md:opacity-50': isHovered && hovered !== i,
-              })}
-            />
-          </div>
-        </a>
+        <div className={p.wrapperClass}>
+          {/* <a
+            href={p.href}
+            target='_blank'
+            rel='noopener noreferrer'
+            key={i}
+            onMouseEnter={() => setHovered(i)}
+            onMouseLeave={() => setHovered(10)}
+            className={p.wrapperClass}
+          > */}
+          <Image
+            alt={p.alt}
+            src={p.src}
+            fill
+            priority
+            className={clsx(p.imgClass, {
+              'md:opacity-50': isHovered && hovered !== i,
+            })}
+            style={p.style}
+          />
+          {/* </a> */}
+        </div>
       ))}
     </div>
   )
--- app/utils/projects.tsx
+++ app/utils/projects.tsx
@@ -7,7 +7,7 @@ import nw from 'public/images/home/nw.jpg'
 
 export default [
   {
-    wrapperClass: 'relative h-40 mb-4',
+    wrapperClass: 'relative w-[40%] h-40 mb-4',
     alt: 'quext',
     src: quext,
     imgClass: 'rounded-lg object-cover',
@@ -15,15 +15,7 @@ export default [
     sizes: '(max-width: 768px) 213px, 33vw',
   },
   {
-    wrapperClass: 'relative h-80 mb-4 sm:mb-0',
-    alt: 'parsley health',
-    src: ph,
-    imgClass: 'rounded-lg d-cover object-cover',
-    href: 'https://parsleyhealth.com',
-    sizes: '(max-width: 768px) 213px, 33vw',
-  },
-  {
-    wrapperClass: 'relative h-40 sm:h-80 sm:mb-4',
+    wrapperClass: 'relative w-[40%] h-40 sm:h-80 sm:mb-4',
     alt: 'delfino',
     src: delfino,
     imgClass: 'rounded-lg object-cover object-top sm:object-center',
@@ -31,7 +23,15 @@ export default [
     sizes: '(max-width: 768px) 213px, 33vw',
   },
   {
-    wrapperClass: 'relative h-40 mb-4 sm:mb-0',
+    wrapperClass: 'relative w-[40%] h-80 mb-4 sm:mb-0',
+    alt: 'parsley health',
+    src: ph,
+    imgClass: 'rounded-lg d-cover object-cover',
+    href: 'https://parsleyhealth.com',
+    sizes: '(max-width: 768px) 213px, 33vw',
+  },
+  {
+    wrapperClass: 'relative w-[40%] h-40 mb-4 sm:mb-0',
     alt: 'nw',
     src: nw,
     imgClass: 'rounded-lg object-cover',
@@ -39,7 +39,7 @@ export default [
     sizes: '(max-width: 768px) 213px, 33vw',
   },
   {
-    wrapperClass: 'relative h-40 mb-4',
+    wrapperClass: 'relative w-[40%] h-40 mb-4',
     alt: 'geeklist',
     src: geeklist,
     imgClass: 'rounded-lg object-cover',
@@ -47,9 +47,10 @@ export default [
     sizes: '(max-width: 768px) 213px, 33vw',
   },
   {
-    wrapperClass: 'relative h-80',
+    wrapperClass: 'relative w-[40%] h-80',
     alt: 'blockmatic',
     src: blockmatic,
+    style: { top: '-160px' },
     imgClass: 'rounded-lg object-cover',
     href: 'https://blockmatic.io',
     sizes: '(max-width: 768px) 213px, 33vw',
