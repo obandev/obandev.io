7013ffba5bec9be1c251f0b1c66447fb9dc03144
jose pablo <josepabloobandogonzalez@gmail.com>
2023-09-30 16:47:54 -0600

    feat: updates link call

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Page() {
           I collaborate with teams to drive software product development,
           guiding technical decisions from concept to production, ensuring
           successful project execution. Here are some projects that I've the
-          privilege of work.
+          privileage of work.
         </p>
       </div>
       <Projects />
@@ -78,7 +78,7 @@ export default function Page() {
           Dm me
         </Link>
 
-        <Link className='ml-4' href='https://cal.com/obandev/obandev'>
+        <Link className='ml-4' href='https://cal.com/obandev'>
           <Svg className='mr-1' viewBox='0 0 20 20'>
             <path d='M11 6.5a.5.5 0 01.5-.5h1a.5.5 0 01.5.5v1a.5.5 0 01-.5.5h-1a.5.5 0 01-.5-.5v-1z' />
             <path d='M3.5 0a.5.5 0 01.5.5V1h8V.5a.5.5 0 011 0V1h1a2 2 0 012 2v11a2 2 0 01-2 2H2a2 2 0 01-2-2V3a2 2 0 012-2h1V.5a.5.5 0 01.5-.5zM1 4v10a1 1 0 001 1h12a1 1 0 001-1V4H1z' />
